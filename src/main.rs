10620f3af3c67e3bbdd65ed028ce1284adf2104c
Jerry Trieu <jerry@advancedtelematic.com>
2015-07-24 10:25:39 +0200

    Fix get rvi_url as args in main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,24 @@ extern crate url;
 
 use sota_client::rvi;
 
+use std::env;
 use std::sync::mpsc::channel;
 use std::thread;
 use url::Url;
 
+/// TODO: Add error handling, remove `unwrap()`
+
 fn main() {
+    let mut args = env::args();
+    args.next();
+    let rvi_url = args.next().unwrap_or(
+        "http://localhost:8901".to_string());
+    let edge_url = args.next().unwrap_or(
+        "http://localhost:18901".to_string());
+
     let rvi_edge = rvi::RviServiceEdge::new(
-        Url::parse("http://localhost:8901").unwrap(),
-        Url::parse("http://localhost:18901").unwrap());
+        Url::parse(rvi_url.as_ref()).unwrap(),
+        Url::parse(edge_url.as_ref()).unwrap());
     rvi_edge.register_service("/sota/notify");
     rvi_edge.register_service("/sota/start");
     rvi_edge.register_service("/sota/chunk");
