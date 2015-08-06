bb468f7392b64c3e7508c4990840e632e08134ee
Philipp Millar <philipp@advancedtelematic.com>
2015-08-06 18:45:33 +0200

    Wrap the IO handling in a HashMap of structs
    This wraps the IO handling in a struct, that holds all the necessary
    state for writing chunks to disk. It introduces a HashMap to hold
    various of those structs, protected by a Mutex and identified by the
    json_rpc message id.

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,18 @@ extern crate sota_client;
 extern crate url;
 
 use sota_client::rvi;
-use sota_client::persistence;
+use sota_client::persistence::PackageFile;
 
 use std::env;
 use std::sync::mpsc::channel;
 use std::thread;
 use url::Url;
-use std::fs::File;
+use std::collections::HashMap;
+use std::collections::hash_map::Entry;
+use std::sync::Mutex;
 
-/// TODO: Add error handling, remove `unwrap()`
-
+// TODO: Add error handling, remove `unwrap()`
+/// Start a SOTA client service listenenig on the provided address/port combinations
 fn main() {
     let mut args = env::args();
     args.next();
@@ -37,22 +39,50 @@ fn main() {
     // TODO: concurrency?
     // TODO: error handling
     // TODO: tests
-    let mut chunk_size: i32 = 0;
-    let mut package_fd: File = File::create("dummy").unwrap();
+    let mut packages = HashMap::new();
 
     loop {
+        // TODO: abstract away the HashMap unwrapping
         let e = rx.recv().unwrap();
         match (e.service_name.as_ref(), e.params) {
             ("/sota/notify", rvi::MessageEventParams::Notify(p)) => {
-                println!("New package available: {}", p.package);
+                println!("New package available: {} with id {}", p.package, e.message_id);
+                let pfile = PackageFile::new(&(p.package), 0, p.retry);
+                packages.insert(e.message_id, Mutex::new(pfile));
             },
             ("/sota/start", rvi::MessageEventParams::Start(p)) => {
-                package_fd = persistence::create_package_fd(&(p.package));
-                chunk_size = p.chunk_size.clone();
+                match packages.entry(e.message_id) {
+                    Entry::Occupied(mut entry) => {
+                        entry.get_mut().lock().unwrap()
+                            .update_chunk_size(p.chunk_size);
+                    }
+                    Entry::Vacant(_) => {
+                        println!("Dropping unnotified start message with id: {}", e.message_id);
+                    }
+                }
             }
             ("/sota/chunk", rvi::MessageEventParams::Chunk(p)) => {
-                let offset = chunk_size * p.index;
-                persistence::write_chunk(&(p.msg), offset as u64, &package_fd);
+                match packages.entry(e.message_id) {
+                    Entry::Occupied(mut entry) => {
+                        entry.get_mut().lock().unwrap()
+                            .write_chunk(&(p.msg), p.index);
+                    }
+                    Entry::Vacant(_) => {
+                        println!("Dropping unnotified chunk message with id: {}", e.message_id);
+                    }
+                }
+            },
+            ("/sota/finish", rvi::MessageEventParams::Finish(_)) => {
+                match packages.entry(e.message_id) {
+                    Entry::Occupied(mut entry) => {
+                        let _ = entry.get_mut().lock().unwrap();
+                    }
+                    Entry::Vacant(_) => {
+                        println!("Dropping unnotified finish message with id: {}", e.message_id);
+                    }
+                }
+                println!("Removing pkg id {}", e.message_id);
+                packages.remove(&e.message_id);
             },
             _ => {}
         }
--- src/persistence.rs
+++ src/persistence.rs
@@ -6,7 +6,7 @@ use std::path::Path;
 
 use rustc_serialize::base64::FromBase64;
 
-pub fn create_package_fd(package_name: &str) -> File {
+fn create_package_fd(package_name: &str) -> File {
     let prefix: String = "/tmp/".to_string();
     let path_string = prefix + package_name;
     let path = Path::new(&path_string);
@@ -19,13 +19,41 @@ pub fn create_package_fd(package_name: &str) -> File {
         .unwrap(); // TODO: error handling
 }
 
-/// Write a base64 encoded chunk with offset `offset` to the provided File
-// TODO: this is slow, rather use a buffered writer and flush on finish?
-pub fn write_chunk(encoded_msg: &str, offset: u64, mut fd: &File) {
-    let decoded_msg = encoded_msg.from_base64().unwrap(); // TODO: error handling
+#[allow(dead_code)]
+pub struct PackageFile {
+    fd: File,
+    chunk_size: i32,
+    retry_count: i32
+}
 
-    // TODO: error checking
-    fd.seek(SeekFrom::Start(offset));
-    fd.write_all(&decoded_msg);
-    fd.flush();
+impl PackageFile {
+    pub fn new(package_name: &str,
+               chunk_size: i32,
+               retry_count: i32) -> PackageFile {
+
+        return PackageFile {
+            fd: create_package_fd(package_name),
+            chunk_size: chunk_size,
+            retry_count: retry_count
+        }
+
+    }
+
+    /// Set a new chunk size for this package
+    pub fn update_chunk_size(&mut self, chunk_size: i32) {
+        self.chunk_size = chunk_size;
+    }
+
+    /// Write a base64 encoded chunk with offset `offset` to the provided File
+    #[allow(unused_must_use)]
+    pub fn write_chunk(&mut self, encoded_msg: &str, index: i32) {
+        let offset: u64 = (self.chunk_size * index) as u64;
+        let decoded_msg = encoded_msg.from_base64().unwrap(); // TODO: error handling
+
+        // TODO: this is slow, rather use a buffered writer and flush on finish?
+        // TODO: error checking
+        self.fd.seek(SeekFrom::Start(offset));
+        self.fd.write_all(&decoded_msg);
+        self.fd.flush();
+    }
 }
