44dfdcedb655779e56de199a2fc016954af07d0b
Philipp Millar <philipp@advancedtelematic.com>
2015-08-06 12:06:40 +0200

    Write received data to disk
    Received data is now written to disk in whatever directory we are in.
    Can't handle multiple transactions currently. It is rather slow, because
    every chunk is fully written to disk before the next chunk is being
    processed. Old data is always overwritten.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
 extern crate sota_client;
 extern crate url;
+extern crate rustc_serialize;
 
 use sota_client::rvi;
 
 use std::env;
 use std::sync::mpsc::channel;
 use std::thread;
+use std::fs::{OpenOptions, File};
+use std::io::{SeekFrom, Seek, Write};
+use std::path::Path;
 use url::Url;
+
+use rustc_serialize::base64::FromBase64;
 
 /// TODO: Add error handling, remove `unwrap()`
 
@@ -32,15 +38,36 @@ fn main() {
         rvi_edge.start(rvi::RviServiceHandler::new(txc));
     });
 
+    // TODO: concurrency?
+    // TODO: error handling
+    // TODO: tests
+    let mut chunk_size: i32 = 0;
+    let mut package_fd: File = File::create("dummy").unwrap();
+
     loop {
         let e = rx.recv().unwrap();
         match (e.service_name.as_ref(), e.params) {
             ("/sota/notify", rvi::MessageEventParams::Notify(p)) => {
-                println!("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
-                println!("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
                 println!("New package available: {}", p.package);
-                println!("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
-                println!("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
+            },
+            ("/sota/start", rvi::MessageEventParams::Start(p)) => {
+                let path = Path::new(&(p.package));
+                package_fd = OpenOptions::new()
+                    .write(true)
+                    .create(true)
+                    .truncate(true)
+                    .open(path)
+                    .unwrap();
+                chunk_size = p.chunk_size.clone();
+            }
+            ("/sota/chunk", rvi::MessageEventParams::Chunk(p)) => {
+                let decoded_msg = p.msg.from_base64().unwrap();
+                let offset = chunk_size * p.index;
+
+                // TODO: this is slow, rather use a buffered writer and flush on finish?
+                package_fd.seek(SeekFrom::Start(offset as u64));
+                package_fd.write_all(&decoded_msg);
+                package_fd.flush();
             },
             _ => {}
         }
