0f13257f850a9b5629471f6160fb998d037772a4
Philipp Millar <philipp@advancedtelematic.com>
2015-10-06 17:52:55 +0200

    Add argument parsing with getopts

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,54 @@
-// TODO: proper argument parsing
-//       See crates docopt or getopts
 extern crate sota_client;
 #[macro_use] extern crate log;
 extern crate env_logger;
+extern crate getopts;
 
 use std::env;
+use getopts::{Options, Matches};
 use sota_client::configuration::Configuration;
 use sota_client::main_loop;
 
 #[cfg_attr(test, allow(dead_code))]
+fn print_usage(program: &str, opts: &Options) {
+    let brief = format!("Usage: {} [options]", program);
+    print!("{}", opts.usage(&brief));
+}
+
+fn match_args(args: &[String], program: &str) -> Matches {
+    let mut options = Options::new();
+    options.optflag("h", "help", "print this help message");
+    options.optopt("c", "config", "change the path where the configuration \
+                   is expected", "FILE");
+    options.optopt("r", "rvi", "explicitly set the URL, where RVI can be \
+                   reached", "URL");
+    options.optopt("e", "edge", "explicitly set the host and port, where the \
+                   client should listen for connections from RVI", "HOST:PORT");
+
+    let matches = match options.parse(args) {
+        Ok(m) => { m }
+        Err(f) => {
+            error!("{}", f.to_string());
+            print_usage(program, &options);
+            std::process::exit(1);
+        }
+    };
+
+    if matches.opt_present("h") {
+        print_usage(program, &options);
+        std::process::exit(0);
+    }
+    matches
+}
+
+#[cfg_attr(test, allow(dead_code))]
 fn main() {
     env_logger::init().unwrap();
+    let args: Vec<String> = env::args().collect();
+    let program: &str = &args[0];
+    let matches = match_args(&args[1..], program);
 
-    let conf_file = Configuration::default_path();
+    let conf_file = matches.opt_str("c")
+        .unwrap_or(Configuration::default_path());
     let configuration = match Configuration::read(&conf_file) {
         Ok(value) => value,
         Err(e) => {
@@ -21,15 +57,12 @@ fn main() {
         }
     };
 
-    let mut args = env::args();
-    args.next();
-
-    let rvi_url: String = args.next().unwrap_or(
-        configuration.client.rvi_url.clone().unwrap_or(
-        "http://localhost:8901".to_string()));
-    let edge_url: String = args.next().unwrap_or(
-        configuration.client.edge_url.clone().unwrap_or(
-        "localhost:18901".to_string()));
+    let rvi_url: String = matches.opt_str("r")
+        .unwrap_or(configuration.client.rvi_url.clone()
+                   .unwrap_or("http://localhost:8901".to_string()));
+    let edge_url: String = matches.opt_str("e")
+        .unwrap_or(configuration.client.edge_url.clone()
+                   .unwrap_or("localhost:9080".to_string()));
 
     main_loop::start(&configuration, rvi_url, edge_url);
 }
