3b5fb687836449c010ed11a41c1c8122773eeb44
Stevan Andjelkovic <stevan@advancedtelematic.com>
2016-03-16 13:37:08 +0100

    Don't use string errors.

--- src/config.rs
+++ src/config.rs
@@ -1,12 +1,13 @@
 use hyper::Url;
 use rustc_serialize::Decodable;
 use std::fs::File;
-use std::io::prelude::*;
 use std::io::ErrorKind;
-use std::io;
+use std::io::prelude::*;
 use toml;
 
 use error::Error;
+use error::ConfigReason::{Parse, Io};
+use error::ParseReason::{InvalidToml, InvalidSection};
 
 
 #[derive(Default, PartialEq, Eq, Debug, Clone)]
@@ -64,20 +65,20 @@ impl Default for TestConfig {
 
 pub fn parse_config(s: &str) -> Result<Config, Error> {
 
-    fn parse_sect<T: Decodable>(tbl: &toml::Table, sect: &str) -> Result<T, Error> {
-        tbl.get(sect)
+    fn parse_sect<T: Decodable>(tbl: &toml::Table, sect: String) -> Result<T, Error> {
+        tbl.get(&sect)
             .and_then(|c| toml::decode::<T>(c.clone()) )
-            .ok_or(Error::ConfigParseError(format!("invalid section: {}", sect)))
+            .ok_or(Error::Config(Parse(InvalidSection(sect))))
     }
 
     let tbl: toml::Table =
         try!(toml::Parser::new(&s)
              .parse()
-             .ok_or(Error::ConfigParseError("invalid toml".to_string())));
+             .ok_or(Error::Config(Parse(InvalidToml))));
 
-    let auth_cfg: AuthConfig = try!(parse_sect(&tbl, "auth"));
-    let ota_cfg:  OtaConfig  = try!(parse_sect(&tbl, "ota"));
-    let test_cfg: TestConfig = try!(parse_sect(&tbl, "test"));
+    let auth_cfg: AuthConfig = try!(parse_sect(&tbl, "auth".to_string()));
+    let ota_cfg:  OtaConfig  = try!(parse_sect(&tbl, "ota".to_string()));
+    let test_cfg: TestConfig = try!(parse_sect(&tbl, "test".to_string()));
 
     return Ok(Config {
         auth: auth_cfg,
@@ -88,18 +89,13 @@ pub fn parse_config(s: &str) -> Result<Config, Error> {
 
 pub fn load_config(path: &str) -> Result<Config, Error> {
 
-    impl From<io::Error> for Error {
-        fn from(err: io::Error) -> Error {
-            Error::ConfigIOError(format!("{}", err))
-        }
-    }
-
     match File::open(path) {
         Err(ref e) if e.kind() == ErrorKind::NotFound => Ok(Config::default()),
-        Err(e) => Err(From::from(e)),
-        Ok(mut f) => {
+        Err(e)                                        => Err(Error::Config(Io(e))),
+        Ok(mut f)                                     => {
             let mut s = String::new();
-            try!(f.read_to_string(&mut s));
+            try!(f.read_to_string(&mut s)
+                 .map_err(|err| Error::Config(Io(err))));
             return parse_config(&s);
         }
     }
@@ -110,9 +106,8 @@ pub fn load_config(path: &str) -> Result<Config, Error> {
 mod tests {
 
     use super::*;
-    use error::Error;
 
-    fn default_config_str() -> &'static str {
+    static DEFAULT_CONFIG_STRING: &'static str =
         r#"
         [auth]
         server = "http://127.0.0.1:9000"
@@ -125,18 +120,18 @@ mod tests {
 
         [test]
         looping = false
-        "#
-    }
+        "#;
 
     #[test]
     fn parse_default_config() {
-        assert_eq!(parse_config(default_config_str()).unwrap(),
+        assert_eq!(parse_config(DEFAULT_CONFIG_STRING).unwrap(),
                    Config::default());
     }
 
     #[test]
     fn bad_path_yields_default_config() {
-        assert_eq!(load_config(""), Ok(Config::default()))
+        assert_eq!(load_config("").unwrap(),
+                   Config::default())
     }
 
 }
