45b46d6626f838b3ede2185013839fdd5fdc462d
Shaun Taheri <shaun@advancedtelematic.com>
2016-04-04 16:40:47 +0200

    Statically link binary and add DEB and RPM targets

--- README.md
+++ README.md
@@ -13,3 +13,19 @@ To build and test the project simply issue:
 
     cargo build
     cargo test
+
+## Packaging instructions
+
+### DEB
+
+A Dockerfile has been set up with the correct libraries for building a statically linked binary. This can be built from the project root with `docker build -t deb-packager pkg/deb`. The DEB package can then be built with `docker run -it --rm -v $PWD:/build deb-packager`.
+
+Alternatively, with the correct build packages installed, `make deb` can be run from the project root.
+
+### RPM
+
+[FPM](https://github.com/jordansissel/fpm) is used to create RPM packages.
+
+A Dockerfile has been set up with the correct libraries and can be built from the project root with `docker build -t rpm-packager pkg/rpm`. An RPM can then be created with `docker run -it --rm -v $PWD:/build rpm-packager`.
+
+Alternatively, assuming FPM and the correct libraries are installed, an RPM can be built with `make rpm`.
--- pkg/pkg.sh
+++ pkg/pkg.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+set -eo pipefail
+
+PKG_NAME="ota-plus-client"
+PKG_VER="0.1.0"
+PKG_DIR="${PKG_NAME}-${PKG_VER}"
+PKG_TARBALL="${PKG_NAME}_${PKG_VER}"
+PREFIX=/opt/ats
+
+cd $(dirname $0)
+PKG_SRC_DIR=$(pwd)
+
 function envsub {
   awk '
 /^\s*(#.*)?$/ {
@@ -17,36 +28,54 @@ function envsub {
 }' $*
 }
 
-if [ $# -lt 1 ]
-then
-  echo "Usage: $0 <DEST>"
+function make_deb {
+  workdir="${TMPDIR:-/tmp}/pkg-ota-plus-client-$$"
+  cp -pr $PKG_SRC_DIR/deb $workdir
+  cd $workdir
+
+  mkdir -p $PKG_DIR/bin
+  mkdir -p $PKG_DIR/etc
+  envsub ${PKG_SRC_DIR}/ota.toml.template > $PKG_DIR/etc/ota.toml
+  tar czf $PKG_TARBALL $PKG_DIR/bin $PKG_DIR/etc
+
+  cd $PKG_DIR
+  debuild -i -us -uc -b
+  mv -n ../ota-plus-client*.deb $dest
+  cd $PKG_SRC_DIR
+  rm -rf $workdir
+}
+
+function make_rpm {
+  cd $PKG_SRC_DIR
+  envsub ota.toml.template > $PKG_NAME.toml
+
+  fpm -s dir -t rpm -n ${PKG_NAME} -v ${PKG_VER} --prefix ${PREFIX} -a native \
+    --rpm-service $PKG_SRC_DIR/rpm/ota-client.service \
+    ../target/release/ota_plus_client=ota_plus_client $PKG_NAME.toml=ota.toml
+
+  mv -n ota-plus-client*.rpm $dest
+  rm $PKG_NAME.toml
+}
+
+if [ $# -lt 2 ]; then
+  echo "Usage: $0 <package> <destination>"
+  echo "packages: deb rpm"
   exit 1
 fi
 
-dest="${1}"
+package="${1}"
+dest="${2}"
+
 echo "Building pkg to '$dest'"
+case $package in
+  "deb" )
+    make_deb
+    ;;
+  "rpm" )
+    make_rpm
+    ;;
+  *)
+    echo "unknown package $package"
+    exit 2
+esac
 
-PKG_NAME="ota-plus-client"
-PKG_VER="0.1.0"
-PKG_DIR="${PKG_NAME}-${PKG_VER}"
-PKG_TARBALL="${PKG_NAME}_${PKG_VER}"
-
-cd `dirname $0`
-PKG_SRC_DIR=`pwd`
-
-workdir="${TMPDIR:-/tmp}/pkg-ota-plus-client-$$"
-cp -pr deb $workdir
-cd $workdir
-
-mkdir -p $PKG_DIR/bin
-mkdir -p $PKG_DIR/etc
-envsub ${PKG_SRC_DIR}/ota.toml.template > $PKG_DIR/etc/ota.toml
-tar czf $PKG_TARBALL $PKG_DIR/bin $PKG_DIR/etc
-
-cd $PKG_DIR
-debuild -i -us -uc -b
-cd ..
-cp ota-plus-client*.deb "${dest}"
-
-cd $PKG_SRC_DIR
-rm -rf $workdir
