6b9207d78874fb6c13bc264358b2034259abe832
Stevan Andjelkovic <stevan@advancedtelematic.com>
2016-04-22 14:55:53 +0200

    Finish interpreter.

--- src/interaction_library/interpreter.rs
+++ src/interaction_library/interpreter.rs
@@ -3,7 +3,7 @@ use std::sync::mpsc::{Sender, Receiver};
 
 pub trait Interpreter<Env, C, E> {
 
-    fn interpret(env: &mut Env, c: C, e: Sender<E>);
+    fn interpret(env: &mut Env, c: C, tx: Sender<E>);
 
     fn run(env: &mut Env, rx: Receiver<C>, tx: Sender<E>) {
         loop {
--- src/new_interpreter.rs
+++ src/new_interpreter.rs
@@ -2,7 +2,8 @@ use std::borrow::Cow;
 use std::process::exit;
 use std::sync::mpsc::Sender;
 
-use datatype::{AccessToken, Command, Config, Error, Event, UpdateState};
+use datatype::{AccessToken, Command, Config, Error, Event, UpdateReport,
+               UpdateRequestId, UpdateState, UpdateResultCode};
 use datatype::Command::*;
 use package_manager::PackageManager;
 use http_client::HttpClient2;
@@ -16,66 +17,83 @@ use new_ota_plus::{get_package_updates, download_package_update,
 pub struct Env<'a> {
     config:       &'a Config,
     access_token: Option<Cow<'a, AccessToken>>,
+
+    // XXX: remove, already in config.
     pkg_manager:  &'a PackageManager,
     http_client:  &'a HttpClient2,
 }
 
-macro_rules! fun0 {
-    ($fun: ident, $env: expr, $token: expr) =>
-        (let $fun = || $fun($env.config, $env.http_client, &$token));
+// This macro partially applies the config and http client to the passed
+// in functions.
+macro_rules! partial_apply {
+    ([ $( $fun0: ident ),* ], [ $( $fun1: ident ),* ], [ $( $fun2: ident ),* ],  $env: expr, $token: expr) => {
+        $(let $fun0 = ||           $fun0($env.config, $env.http_client, $token);)*;
+        $(let $fun1 = |arg|        $fun1($env.config, $env.http_client, $token, &arg);)*;
+        $(let $fun2 = |arg1, arg2| $fun2($env.config, $env.http_client, $token, &arg1, &arg2);)*;
+    }
 }
 
-macro_rules! fun1 {
-    ($fun: ident, $env: expr, $token: expr) =>
-        (let $fun = |arg| $fun($env.config, $env.http_client, &$token, &arg));
+// XXX: Move this somewhere else?
+fn install_package_update(config: &Config,
+                          http_client: &HttpClient2,
+                          token: &AccessToken,
+                          id: &UpdateRequestId,
+                          tx: &Sender<Event>) -> Result<UpdateReport, Error> {
+
+    match download_package_update(config, http_client, token, id) {
+
+        Ok(path) => {
+            info!("Downloaded at {:?}. Installing...", path);
+            try!(tx.send(Event::UpdateStateChanged(id.clone(), UpdateState::Installing)));
+
+            let p = try!(path.to_str()
+                         .ok_or(Error::ParseError(format!("Path is not valid UTF-8: {:?}", path))));
+
+            match config.ota.package_manager.install_package(p) {
+
+                Ok((code, output)) => {
+                    try!(tx.send(Event::UpdateStateChanged(id.clone(), UpdateState::Installed)));
+                    Ok(UpdateReport::new(id.clone(), code, output))
+                }
+
+                Err((code, output)) => {
+                    try!(tx.send(Event::UpdateErrored(id.clone(), format!("{:?}: {:?}", code, output))));
+                    Ok(UpdateReport::new(id.clone(), code, output))
+                }
+
+            }
+
+        }
+
+        Err(err) => {
+            try!(tx.send(Event::UpdateErrored(id.clone(), format!("{:?}", err))));
+            Ok(UpdateReport::new(id.clone(),
+                              UpdateResultCode::GENERAL_ERROR,
+                              format!("Download failed: {:?}", err)))
+        }
+    }
+
 }
 
-fn interpreter(env: &mut Env, cmd: Command, rx: &Sender<Event>) -> Result<(), Error> {
+fn interpreter(env: &mut Env, cmd: Command, tx: &Sender<Event>) -> Result<(), Error> {
 
     Ok(if let Some(token) = env.access_token.to_owned() {
 
-        fun0!(get_package_updates,     &env, &token);
-        fun1!(post_packages,           &env, &token);
-        fun1!(download_package_update, &env, &token);
-        fun1!(send_install_report,     &env, &token);
+        partial_apply!(
+            [get_package_updates],
+            [post_packages, send_install_report],
+            [install_package_update], &env, &token);
 
         match cmd {
-            Authenticate(_)       => (),
+
+            Authenticate(_)       => (), // Already authenticated.
+
             AcceptUpdate(ref id)  => {
-
-                try!(rx.send(Event::UpdateStateChanged(id.clone(), UpdateState::Downloading)));
-
+                try!(tx.send(Event::UpdateStateChanged(id.clone(), UpdateState::Downloading)));
+                let report = try!(install_package_update(id.to_owned(), tx.to_owned()));
+                try!(send_install_report(report.clone()));
+                info!("Update finished. Report sent: {:?}", report)
             }
-/*
-    fn accept_update(&self, id: &UpdateRequestId) {
-        let report = download_package_update::<C>(&self.token, &self.config, id)
-            .and_then(|path| {
-                info!("Downloaded at {:?}. Installing...", path);
-                self.publish(Event::UpdateStateChanged(id.clone(), UpdateState::Installing));
-
-                let p = try!(path.to_str().ok_or(Error::ParseError(format!("Path is not valid UTF-8: {:?}", path))));
-                self.config.ota.package_manager.install_package(p)
-                    .map(|(code, output)| {
-                        self.publish(Event::UpdateStateChanged(id.clone(), UpdateState::Installed));
-                        UpdateReport::new(id.clone(), code, output)
-                    })
-                    .or_else(|(code, output)| {
-                        self.publish(Event::UpdateErrored(id.clone(), format!("{:?}: {:?}", code, output)));
-                        Ok(UpdateReport::new(id.clone(), code, output))
-                    })
-            }).unwrap_or_else(|e| {
-                self.publish(Event::UpdateErrored(id.clone(), format!("{:?}", e)));
-                UpdateReport::new(id.clone(),
-                                   UpdateResultCode::GENERAL_ERROR,
-                                   format!("Download failed: {:?}", e))
-            });
-
-        match send_install_report::<C>(&self.token, &self.config, &report) {
-            Ok(_) => info!("Update finished. Report sent: {:?}", report),
-            Err(e) => error!("Error reporting back to the server: {:?}", e)
-        }
-    }
-*/
 
             GetPendingUpdates     => {
                 let updates = try!(get_package_updates());
@@ -84,12 +102,12 @@ fn interpreter(env: &mut Env, cmd: Command, rx: &Sender<Event>) -> Result<(), Er
                     .map(|id| Event::NewUpdateAvailable(id.clone()))
                     .collect();
                 info!("New package updates available: {:?}", update_events);
-                try!(rx.send(Event::Batch(update_events)))
+                try!(tx.send(Event::Batch(update_events)))
             }
 
             ListInstalledPackages => {
                 let pkgs = try!(env.config.ota.package_manager.installed_packages());
-                try!(rx.send(Event::FoundInstalledPackages(pkgs.clone())))
+                try!(tx.send(Event::FoundInstalledPackages(pkgs.clone())))
             }
 
             PostInstalledPackages => {
@@ -117,7 +135,7 @@ fn interpreter(env: &mut Env, cmd: Command, rx: &Sender<Event>) -> Result<(), Er
             GetPendingUpdates     |
             ListInstalledPackages |
             PostInstalledPackages         =>
-                rx.send(Event::NotAuthenticated)
+                tx.send(Event::NotAuthenticated)
                   .unwrap_or(error!("not_auth: send failed."))
             }
 
@@ -129,9 +147,9 @@ pub struct OurInterpreter;
 
 impl<'a> Interpreter<Env<'a>, Command, Event> for OurInterpreter {
 
-    fn interpret(env: &mut Env, cmd: Command, rx: Sender<Event>) {
-        interpreter(env, cmd, &rx)
-            .unwrap_or_else(|err| rx.send(Event::Error(format!("{}", err)))
+    fn interpret(env: &mut Env, cmd: Command, tx: Sender<Event>) {
+        interpreter(env, cmd, &tx)
+            .unwrap_or_else(|err| tx.send(Event::Error(format!("{}", err)))
                             .unwrap_or(error!("interpret: send failed.")))
     }
 
