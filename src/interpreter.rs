1a408082b3b49849886ae8dd751f012e205b3559
Stevan Andjelkovic <stevan@advancedtelematic.com>
2016-04-28 14:55:19 +0200

    Move install_package_update to ota module.

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -4,12 +4,12 @@ use std::sync::{Arc, Mutex};
 use std::sync::mpsc::Sender;
 
 use auth_plus::authenticate;
-use datatype::{AccessToken, Command, Config, Error, Event, UpdateReport,
-               UpdateRequestId, UpdateState, UpdateResultCode};
+use datatype::{AccessToken, Command, Config, Error, Event, UpdateState};
 use datatype::Command::*;
 use http_client::HttpClient;
 use interaction_library::interpreter::Interpreter;
-use ota_plus::{get_package_updates, download_package_update, post_packages, send_install_report};
+use ota_plus::{get_package_updates, install_package_update,
+               post_installed_packages, send_install_report};
 
 
 pub struct Env<'a> {
@@ -21,61 +21,14 @@ pub struct Env<'a> {
 // This macro partially applies the config, http client and token to the
 // passed in functions.
 macro_rules! partial_apply {
-    ([ $( $fun0: ident ),* ],
-     [ $( $fun1: ident ),* ],
-     [ $( $fun2: ident ),* ],
+    ([ $( $fun0: ident ),* ], // Functions of arity 0,
+     [ $( $fun1: ident ),* ], // arity 1,
+     [ $( $fun2: ident ),* ], // and arity 2.
      $config: expr, $client: expr, $token: expr) => {
         $(let $fun0 = ||           $fun0(&$config, &mut *$client.lock().unwrap(), $token);)*
         $(let $fun1 = |arg|        $fun1(&$config, &mut *$client.lock().unwrap(), $token, &arg);)*
         $(let $fun2 = |arg1, arg2| $fun2(&$config, &mut *$client.lock().unwrap(), $token, &arg1, &arg2);)*
     }
-}
-
-// XXX: Move this somewhere else?
-pub fn install_package_update(config:      &Config,
-                              http_client: &mut HttpClient,
-                              token:       &AccessToken,
-                              id:          &UpdateRequestId,
-                              tx:          &Sender<Event>) -> Result<UpdateReport, Error> {
-
-    match download_package_update(config, http_client, token, id) {
-
-        Ok(path) => {
-            info!("Downloaded at {:?}. Installing...", path);
-            try!(tx.send(Event::UpdateStateChanged(id.clone(), UpdateState::Installing)));
-
-            let p = try!(path.to_str()
-                         .ok_or(Error::ParseError(format!("Path is not valid UTF-8: {:?}", path))));
-
-            match config.ota.package_manager.install_package(p) {
-
-                Ok((code, output)) => {
-                    try!(tx.send(Event::UpdateStateChanged(id.clone(), UpdateState::Installed)));
-
-                    // XXX: Slight code duplication, see interpret(PostInstalledPackages).
-                    let pkgs = try!(config.ota.package_manager.installed_packages());
-                    try!(post_packages(config, http_client, token, &pkgs));
-
-                    Ok(UpdateReport::new(id.clone(), code, output))
-                }
-
-                Err((code, output)) => {
-                    try!(tx.send(Event::UpdateErrored(id.clone(), format!("{:?}: {:?}", code, output))));
-                    Ok(UpdateReport::new(id.clone(), code, output))
-                }
-
-            }
-
-        }
-
-        Err(err) => {
-            try!(tx.send(Event::UpdateErrored(id.clone(), format!("{:?}", err))));
-            Ok(UpdateReport::new(id.clone(),
-                              UpdateResultCode::GENERAL_ERROR,
-                              format!("Download failed: {:?}", err)))
-        }
-    }
-
 }
 
 fn interpreter(env: &mut Env, cmd: Command, tx: &Sender<Event>) -> Result<(), Error> {
@@ -85,8 +38,8 @@ fn interpreter(env: &mut Env, cmd: Command, tx: &Sender<Event>) -> Result<(), Er
         let client_clone = env.http_client.clone();
 
         partial_apply!(
-            [get_package_updates],
-            [post_packages, send_install_report],
+            [get_package_updates, post_installed_packages],
+            [send_install_report],
             [install_package_update], &env.config, client_clone, &token);
 
         match cmd {
@@ -116,9 +69,7 @@ fn interpreter(env: &mut Env, cmd: Command, tx: &Sender<Event>) -> Result<(), Er
             }
 
             PostInstalledPackages => {
-                let pkgs = try!(env.config.ota.package_manager.installed_packages());
-                debug!("Found installed packages in the system: {:?}", pkgs);
-                try!(post_packages(pkgs));
+                try!(post_installed_packages());
                 info!("Posted installed packages to the server.")
             }
 
@@ -159,74 +110,6 @@ impl<'a> Interpreter<Env<'a>, Command, Event> for OurInterpreter {
         interpreter(env, cmd, &tx)
             .unwrap_or_else(|err| tx.send(Event::Error(format!("{}", err)))
                             .unwrap_or(error!("interpret: send failed.")))
-    }
-
-}
-
-#[cfg(test)]
-mod tests {
-
-    use std::fmt::Debug;
-    use std::sync::mpsc::{channel, Receiver};
-
-    use super::*;
-    use datatype::{AccessToken, Config, Event, UpdateResultCode, UpdateState};
-    use http_client::TestHttpClient;
-
-    fn assert_receiver_eq<X: PartialEq + Debug>(rx: Receiver<X>, xs: &[X]) {
-
-        let mut xs = xs.iter();
-
-        while let Ok(x) = rx.try_recv() {
-            if let Some(y) = xs.next() {
-                assert_eq!(x, *y)
-            } else {
-                panic!("assert_receiver_eq: never nexted `{:?}`", x)
-            }
-        }
-
-        if let Some(x) = xs.next() {
-            panic!("assert_receiver_eq: never received `{:?}`", x)
-        }
-
-    }
-
-    #[test]
-    fn test_install_package_update_0() {
-
-        let (tx, rx) = channel();
-
-        assert_eq!(install_package_update(
-            &Config::default(),
-            &mut TestHttpClient::new(),
-            &AccessToken::default(),
-            &"0".to_string(),
-            &tx).unwrap().operation_results.pop().unwrap().result_code,
-                   UpdateResultCode::GENERAL_ERROR);
-
-        assert_receiver_eq(rx, &[
-            Event::UpdateErrored("0".to_string(), String::from(
-                "ClientError(\"GET http://127.0.0.1:8080/api/v1/vehicles/V1234567890123456/updates/0/download\")"))])
-
-    }
-
-    #[test]
-    fn test_install_package_update_1() {
-
-        let (tx, rx) = channel();
-
-        assert_eq!(install_package_update(
-            &Config::default(),
-            &mut TestHttpClient::from(vec![""]),
-            &AccessToken::default(),
-            &"0".to_string(),
-            &tx).unwrap().operation_results.pop().unwrap().result_code,
-                   UpdateResultCode::INSTALL_FAILED);
-
-        assert_receiver_eq(rx, &[
-            Event::UpdateStateChanged("0".to_string(), UpdateState::Installing),
-            Event::UpdateErrored("0".to_string(), "INSTALL_FAILED: \"\"".to_string())])
-
     }
 
 }
--- src/ota_plus.rs
+++ src/ota_plus.rs
@@ -1,9 +1,12 @@
 use rustc_serialize::json;
 use std::fs::File;
 use std::path::PathBuf;
+use std::sync::mpsc::Sender;
 
-use datatype::{AccessToken, Config, Error, Url, UpdateRequestId,
-               UpdateReport, UpdateReportWithVin, Package};
+use datatype::{AccessToken, Config, Event, Error, Url, UpdateRequestId,
+               UpdateReport, UpdateReportWithVin, Package, UpdateState,
+               UpdateResultCode};
+
 use http_client::{Auth, HttpClient, HttpRequest};
 
 
@@ -69,9 +72,7 @@ pub fn get_package_updates(config: &Config,
 
 }
 
-// XXX: This function is only used for posting installed packages? If
-// so, then it might as well get those from the package manager directly
-// (which is part of config).
+// XXX: Remove in favour of post_installed_packages()?
 pub fn post_packages(config: &Config,
                      client: &mut HttpClient,
                      token:  &AccessToken,
@@ -90,12 +91,68 @@ pub fn post_packages(config: &Config,
     return Ok(())
 }
 
+pub fn post_installed_packages(config: &Config,
+                               client: &mut HttpClient,
+                               token:  &AccessToken) -> Result<(), Error> {
+
+    let pkgs = try!(config.ota.package_manager.installed_packages());
+    post_packages(config, client, token, &pkgs)
+
+}
+
+pub fn install_package_update(config:      &Config,
+                              http_client: &mut HttpClient,
+                              token:       &AccessToken,
+                              id:          &UpdateRequestId,
+                              tx:          &Sender<Event>) -> Result<UpdateReport, Error> {
+
+    match download_package_update(config, http_client, token, id) {
+
+        Ok(path) => {
+            info!("Downloaded at {:?}. Installing...", path);
+            try!(tx.send(Event::UpdateStateChanged(id.clone(), UpdateState::Installing)));
+
+            let p = try!(path.to_str()
+                         .ok_or(Error::ParseError(format!("Path is not valid UTF-8: {:?}", path))));
+
+            match config.ota.package_manager.install_package(p) {
+
+                Ok((code, output)) => {
+                    try!(tx.send(Event::UpdateStateChanged(id.clone(), UpdateState::Installed)));
+                    try!(post_installed_packages(config, http_client, token));
+                    Ok(UpdateReport::new(id.clone(), code, output))
+                }
+
+                Err((code, output)) => {
+                    try!(tx.send(Event::UpdateErrored(id.clone(), format!("{:?}: {:?}", code, output))));
+                    Ok(UpdateReport::new(id.clone(), code, output))
+                }
+
+            }
+
+        }
+
+        Err(err) => {
+            try!(tx.send(Event::UpdateErrored(id.clone(), format!("{:?}", err))));
+            Ok(UpdateReport::new(id.clone(),
+                              UpdateResultCode::GENERAL_ERROR,
+                              format!("Download failed: {:?}", err)))
+        }
+    }
+
+}
+
+
 
 #[cfg(test)]
 mod tests {
 
+    use std::fmt::Debug;
+    use std::sync::mpsc::{channel, Receiver};
+
     use super::*;
-    use datatype::{AccessToken, Config, OtaConfig, Package};
+    use datatype::{AccessToken, Config, Event, OtaConfig, Package,
+                   UpdateResultCode, UpdateState};
     use http_client::TestHttpClient;
 
 
@@ -154,6 +211,62 @@ mod tests {
                                                    &"0".to_string())
                            .unwrap_err()),
                    "Http client error: GET http://127.0.0.1:8080/api/v1/vehicles/V1234567890123456/updates/0/download")
+    }
+
+    fn assert_receiver_eq<X: PartialEq + Debug>(rx: Receiver<X>, xs: &[X]) {
+
+        let mut xs = xs.iter();
+
+        while let Ok(x) = rx.try_recv() {
+            if let Some(y) = xs.next() {
+                assert_eq!(x, *y)
+            } else {
+                panic!("assert_receiver_eq: never nexted `{:?}`", x)
+            }
+        }
+
+        if let Some(x) = xs.next() {
+            panic!("assert_receiver_eq: never received `{:?}`", x)
+        }
+
+    }
+
+    #[test]
+    fn test_install_package_update_0() {
+
+        let (tx, rx) = channel();
+
+        assert_eq!(install_package_update(
+            &Config::default(),
+            &mut TestHttpClient::new(),
+            &AccessToken::default(),
+            &"0".to_string(),
+            &tx).unwrap().operation_results.pop().unwrap().result_code,
+                   UpdateResultCode::GENERAL_ERROR);
+
+        assert_receiver_eq(rx, &[
+            Event::UpdateErrored("0".to_string(), String::from(
+                "ClientError(\"GET http://127.0.0.1:8080/api/v1/vehicles/V1234567890123456/updates/0/download\")"))])
+
+    }
+
+    #[test]
+    fn test_install_package_update_1() {
+
+        let (tx, rx) = channel();
+
+        assert_eq!(install_package_update(
+            &Config::default(),
+            &mut TestHttpClient::from(vec![""]),
+            &AccessToken::default(),
+            &"0".to_string(),
+            &tx).unwrap().operation_results.pop().unwrap().result_code,
+                   UpdateResultCode::INSTALL_FAILED);
+
+        assert_receiver_eq(rx, &[
+            Event::UpdateStateChanged("0".to_string(), UpdateState::Installing),
+            Event::UpdateErrored("0".to_string(), "INSTALL_FAILED: \"\"".to_string())])
+
     }
 
 }
