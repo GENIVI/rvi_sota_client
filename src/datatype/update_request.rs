04f658ea8defc04b14a4ea223c3f98406529e9ed
Simão Mata <simao@advancedtelematic.com>
2016-05-09 14:11:47 +0200

    sort packages by installPos

--- src/datatype/update_request.rs
+++ src/datatype/update_request.rs
@@ -14,6 +14,7 @@ pub enum UpdateState {
 #[allow(non_snake_case)]
 pub struct PendingUpdateRequest {
     pub id:     UpdateRequestId,
+    pub installPos: i32,
     pub packageId: Package,
     pub createdAt: String
 }
