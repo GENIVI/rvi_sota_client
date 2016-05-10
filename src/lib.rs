77baf24a2e7b7489f1cab0e7a192acaf49578ea6
Shaun Taheri <shaun@advancedtelematic.com>
2016-05-10 15:35:22 +0200

    Remove old hyper forwarding tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
 extern crate hyper;
 #[macro_use] extern crate log;
-#[cfg(test)] #[macro_use] extern crate yup_hyper_mock as hyper_mock;
 extern crate rustc_serialize;
 extern crate tempfile;
 extern crate toml;
