41a1023dd4c0884679243aa7474badd6071a8379
Alex Humphreys <alex@advancedtelematic.com>
2016-08-25 17:24:49 +0200

    Add hidden_service hostname to system_info

--- run/system_info.sh
+++ run/system_info.sh
@@ -5,7 +5,13 @@ INFO=$(lshw -json -sanitize)
 MANIFEST_PATH=/etc/manifest.xml
 if [ -f $MANIFEST_PATH ]; then
   MANIFEST={\"manifest_file\":\"$( cat $MANIFEST_PATH | sed 's/"/\\"/'g | sed -e ':a' -e 'N' -e '$!ba' -e 's/\n/ /g' )\"}
-  echo $MANIFEST $INFO | jq -s add | jq -r .
-else
-  echo $INFO | jq -r .
+  INFO=$(echo $MANIFEST $INFO | jq -s add | jq -r .)
 fi
+
+SERVICE_HOSTNAME_PATH=/var/lib/tor/hidden_service/hostname
+if [ -f $SERVICE_HOSTNAME_PATH ]; then
+  SERVICE_HOSTNAME={\"service_hostname\":\"$( cat $SERVICE_HOSTNAME_PATH )\"}
+  INFO=$(echo $SERVICE_HOSTNAME $INFO | jq -s add | jq -r .)
+fi
+
+echo $INFO | jq -r .
